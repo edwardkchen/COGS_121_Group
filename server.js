6123ac8862536c70011554f549ec57b7dc50cff4
EC2 Default User <ec2-user@ip-172-31-18-226.us-east-2.compute.internal>
2018-04-23 15:31:41 +0000

    all the buttons works properly now

--- server.js
+++ server.js
@@ -6,33 +6,42 @@ const path = require('path');
 var http = require('http');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'code')));
+//app.use(express.static(path.join(__dirname, 'code')));
 
 app.get('/', (req, res) => {
   console.log('Running into index page!');
   res.sendFile(path.join(__dirname + '/code/index.html'));
 });
+
 app.get('/friends', (req, res) => {
   console.log('Running into friends page!');
-  res.sendFile(path.join(__dirname + '/code/Friends.html'));
+  res.sendFile(path.join(__dirname + '/code/friends.html'));
 });
+
 app.get('/goals', (req, res) => {
   console.log('Running into goals page!');
   res.sendFile(path.join(__dirname + '/code/goals.html'));
 });
+
 app.get('/house', (req, res) => {
   console.log('Running into house page!');
   res.sendFile(path.join(__dirname + '/code/house.html'));
 });
+
 app.get('/profile', (req, res) => {
   console.log('Running into profile page!');
-  res.sendFile(path.join(__dirname + '/code/Profile.html'));
+  res.sendFile(path.join(__dirname + '/code/profile.html'));
 });
+
 app.get('/feed', (req, res) => {
   console.log('Running into feed page!');
   res.sendFile(path.join(__dirname + '/code/feed.html'));
 });
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
   console.log('Server started!');
+});*/
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started");
 });
