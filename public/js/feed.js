a77bf18bb3c3820bc96399c72e65227a0bb07583
Edward <35312271+edwardkhchen@users.noreply.github.com>
2018-05-30 11:19:59 -0700

    create feed.js

---
+++ public/js/feed.js
@@ -0,0 +1,9 @@
+function smallfeed() {
+document.getElementById("myProgress").value +="10"; 
+}
+function mediumfeed() {
+document.getElementById("myProgress").value += "25";
+}
+function largefeed() {
+document.getElementById("myProgress").value += "33";
+}
