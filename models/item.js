b249409955e820ff37471d80349853638699edc7
Ateyib <aabdulka@ucsd.edu>
2018-06-03 16:38:05 -0700

    added shop in home page

---
+++ models/item.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var ItemSchema = new mongoose.Schema({
+    item_name: String,
+    points: Number,
+    image: String
+  });
+
+module.exports = mongoose.model('Item', UserSchema);
